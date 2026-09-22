9a7a0ea85f33113cfbdba3e7141a9cd554214dcc
MarzukhAsjad <marz.asjad00@gmail.com>
2026-09-22 18:35:54 +0600

    Add slog for /internal go files

--- ajagent/cmd/main.go
+++ ajagent/cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import ajagent "ajagent/pkg"
+import (
+	ajagent "ajagent/pkg"
+	"log/slog"
+	"os"
+)
 
 func main() {
+	slog.SetDefault(slog.New(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			AddSource: true,
+		}),
+    ))
 	ajagent.RunnerAgent()
 }
